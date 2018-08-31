8d2136781f3ee5be831bfb28662678a89d690863
WuJun <wujun19890209@gmail.com>
2018-08-31 22:42:09 +0800

    feat: update favicon

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -2,16 +2,16 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
   siteTitle: 'WuJun', // Navigation and Site Title
-  siteTitleAlt: 'WuJun - Gatsby Starter', // Alternative Site title for SEO
-  siteUrl: 'https://minimal-blog.netlify.com', // Domain of your site. No trailing slash!
+  siteTitleAlt: 'WuJun', // Alternative Site title for SEO
+  siteUrl: 'https://silly-bhaskara-38f6ef.netlify.com/', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
   siteDescription: 'WuJun', // Your site description
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@minimal', // Twitter Username - Optional
-  ogSiteName: 'minimal', // Facebook Site Name - Optional
+  userTwitter: '@freewinginsky', // Twitter Username - Optional
+  ogSiteName: 'WuJun', // Facebook Site Name - Optional
 
   // Manifest and Progress color
   // See: https://developers.google.com/web/fundamentals/web-app-manifest/
