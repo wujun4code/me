557ffce2960e95b8ed559ded9c9fe2ccde4e0621
WuJun <wujun19890209@gmail.com>
2018-09-04 17:02:35 +0800

    fix: name spaces.

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -1,17 +1,17 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'WuJun', // Navigation and Site Title
-  siteTitleAlt: 'WuJun', // Alternative Site title for SEO
+  siteTitle: 'Wu Jun', // Navigation and Site Title
+  siteTitleAlt: 'Wu Jun', // Alternative Site title for SEO
   siteUrl: 'https://silly-bhaskara-38f6ef.netlify.com/', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'WuJun', // Your site description
+  siteDescription: 'Wu Jun', // Your site description
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@freewinginsky', // Twitter Username - Optional
-  ogSiteName: 'WuJun', // Facebook Site Name - Optional
+  ogSiteName: 'Wu Jun', // Facebook Site Name - Optional
 
   // Manifest and Progress color
   // See: https://developers.google.com/web/fundamentals/web-app-manifest/
